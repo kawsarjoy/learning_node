233d31f328af9e64bfdae966bf8ba153c5988614
Mohammad Abu Kawsar <kawsar@linkstaff.co.jp>
2019-04-15 09:41:21 +0600

    change for node 11

--- reference/url_demo.js
+++ reference/url_demo.js
@@ -1,8 +1,8 @@
 const url = require('url');
 
-//const myUrl = new URL('http://mywebsite.com/hello.html?id=100&status=active');
+const myUrl = new URL('http://mywebsite.com/hello.html?id=100&status=active');
 
-const myUrl = url.parse('http://mywebsite.com/hello.html?id=100&status=active');
+//const myUrl = url.parse('http://mywebsite.com/hello.html?id=100&status=active');
 
 // serialize url
 
@@ -11,23 +11,23 @@ console.log(myUrl.toString());
 
 // host (root domain)
 
-console.log(myUrl.host());
+console.log(myUrl.host);
 
 // hostname (does not get port)
 
-console.log(myUrl.hostname());
+console.log(myUrl.hostname);
 
 // pathname
 
-console.log(myUrl.pathname());
+console.log(myUrl.pathname);
 // serialized query
 
-console.log(myUrl.search());
+console.log(myUrl.search);
 // params object
 
-console.log(myUrl.searchParams());
+console.log(myUrl.searchParams);
 // add param
 
 console.log(myUrl.searchParams.append('abc', '123'));
 
-myUrl.searchParams.forEach((value, name) => console.log(`${name}: ${valeu}`));
+myUrl.searchParams.forEach((value, name) => console.log(`${name}: ${value}`));
